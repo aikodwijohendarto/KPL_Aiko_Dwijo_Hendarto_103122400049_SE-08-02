1d4a91cc8751f706bc8af456c350d80df1fb7856
aikodwijohendarto <aikodwijohendarto@gmail.com>
2026-03-03 20:59:12 +0700

    feat: ubah input array untuk pengujian TP 02

--- 02_Pemrograman_JavaScript/index.js
+++ 02_Pemrograman_JavaScript/index.js
@@ -1,16 +1,14 @@
-const arr1 = [1, -2, 3, -4, 5, -6];
+const arr1 = [2, 26, 28, -2];
 
-function mulOfArray(arr) {
+function sumOfArray(arr) {
     let result = 1;
     for (let i = 0; i < arr.length; i = i + 1) {
        if (arr[i] >= 0) {
-           result = result * arr[i];
+           result = result * arr[i]
        }
     }
     
     return result;
 }
 
-// Panggil fungsinya
-const arr1Result = mulOfArray(arr1);
-console.log(arr1Result);
+console.log(sumOfArray(arr1));
