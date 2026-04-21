c7a25d7d85c3d0047e7373713ce874ffa733cffd
aikodwijohendarto <aikodwijohendarto@gmail.com>
2026-04-22 00:13:00 +0700

    Modul 6

---
+++ 06_Design_by_Contract/TM/index.js
@@ -0,0 +1,21 @@
+function is_not_fizzbuzz(number) {
+    if (typeof number !== 'number' || !Number.isFinite(number) || !Number.isInteger(number)) {
+        throw new TypeError('Input harus bilangan bulat yang valid');
+    }
+
+    if (number % 3 === 0 || number % 5 === 0) {
+        return false;
+    }
+
+    return true;
+}
+
+console.log(is_not_fizzbuzz(1))   // true
+console.log(is_not_fizzbuzz(3))   // false
+console.log(is_not_fizzbuzz(5))   // false
+console.log(is_not_fizzbuzz(30))  // false
+console.log(is_not_fizzbuzz(7))   // true
+
+console.log(is_not_fizzbuzz(null))      // TypeError
+console.log(is_not_fizzbuzz(NaN))       // TypeError
+console.log(is_not_fizzbuzz(Infinity))  // TypeError
